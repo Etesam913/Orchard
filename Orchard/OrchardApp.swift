24b136df43de25d7184530fde159101bd8cbd290
Etesam <etesamansari13@gmail.com>
2026-05-17 18:20:34 -0400

    Initial Commit

---
+++ Orchard/OrchardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrchardApp.swift
+//  Orchard
+//
+//  Created by etesam on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct OrchardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
